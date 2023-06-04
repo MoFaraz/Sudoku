65fd6427c20618483e80d981a39b1599b4034ad2
mopharaz <mopharaz@gmail.com>
2023-06-04 12:26:45 +0330

    add some function to cell class

--- js/cell.js
+++ js/cell.js
@@ -1,5 +1,7 @@
 export class Cell {
     mrv = []
+    flag = true
+    isFirst = false
     constructor(value, row, col) {
         this.col = col;
         this.value = value
@@ -17,9 +19,27 @@ export class Cell {
     getRow() {
         return this.row
     }
+
+    setValue(value) {
+        this.value = value
+    }
+
+    getIsFirst() {
+        return this.isFirst
+    }
+    setIsFirst(isFirst) {
+        this.isFirst = isFirst
+    }
+    getFlag() {
+        return this.flag
+    }
+    setFlag(flag) {
+        this.flag = flag
+    }
     calculateMrv() {
+        this.mrv = []
         if (this.value === 0)
-            for (let i = 1 ; i <= gridSize(); i++)
+            for (let i = 1; i <= gridSize(); i++)
                 this.mrv.push(i)
     }
     getMrv() {
